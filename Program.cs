0645aafae1b142625c4dd002e041c6c1393921fa
illyakich <speedfeed123@gmail.com>
2024-09-18 22:05:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Please input a number");
+int Clock = Convert.ToInt32(Console.ReadLine());
+Clock = Clock % 2;
+if (Clock == 0)
+{
+    Console.WriteLine("Tick");  
+}
+else
+{
+    Console.WriteLine("Tock");
+}
+// basically displays "Tick" if the number is even and "Tock" if its odd.
